469cba90d781e2023fc33ac86d21f4dca63209bc
Kacsa <toth00peter@gmail.com>
2023-03-28 14:52:00 +0200

    added migration UwU

---
+++ Etel/Migrations/20230328124936_initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Etel.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Foods",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nev = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tapertek = table.Column<int>(type: "int", nullable: false),
+                    Kategoria = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ar = table.Column<int>(type: "int", nullable: false),
+                    ContentType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Data = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Foods", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Foods");
+        }
+    }
+}
