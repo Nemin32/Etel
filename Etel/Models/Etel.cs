581ef4cdbdee89a8c7e324501c68b2a285850bfb
Perger Lóránt <pergerlori@gmail.com>
2023-03-28 13:38:46 +0200

    Etel model alap.

---
+++ Etel/Models/Etel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Etel.Models
+{
+    public class Etel
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        string Nev { get; set; }
+        [Required]
+        int Tapertek { get; set; }
+        
+        [Required]
+        string Kategoria { get; set; }
+        [Required]
+        int Ar { get; set; }
+        public string? ContentType { get; set; }
+        public byte[]? Data { get; set; }
+
+        public Etel()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
